ff93c91830b4b4413b9b28fad912ade5cf9af32d
Teddyburgonde <bandama.teddy@gmail.com>
2026-01-26 13:59:42 +0100

    Modified new badass

--- badass/projet/new_p3/scripts/vxlan_vtep3.sh
+++ badass/projet/new_p3/scripts/vxlan_vtep3.sh
@@ -1,40 +1,18 @@
 #!/bin/sh
 
 # Créer l'interface VXLAN avec VNI 10
-ip link add vxlan10 type vxlan id 10 local 1.1.1.4 destport 4789 nolearning
+ip link add vxlan10 type vxlan id 10 local 1.1.1.4 dstport 4789 nolearning
 
 # Activer l'interface VXLAN 
 ip link set dev vxlan10 up
 
 # Créer le bridge
-ip link // a continuer 
-
-ip link add vxlan0 type vxlan id 42 local 1.1.1.4
-ip link set dev vxlan0 up
-ip link add br0 type bridge
-ip link set br0 up
-ip link set dev vxlan0 master br0
-ip link set dev eth0 master br0
-ip addr add 172.16.1.4/24 dev br0
-
-
-
-
-#!/bin/sh
-
-# Créer l'interface VXLAN avec VNI 10
-ip link add vxlan10 type vxlan id 10 local 1.1.1.3 dstport 4789 nolearning
-
-# Activer l'interface VXLAN
-ip link set dev vxlan10 up
-
-# Créer le bridge 
 ip link add br0 type bridge
 ip link set br0 up
 
 # Ajouter VXLAN au bridge
 ip link set dev vxlan10 master br0
 
-# Ajouter eth1 (interface vers le Host) au bridge
+# Ajouter eth1(interface vers le Host) au bridge
 ip link set dev eth1 master br0
 ip link set dev eth1 up
