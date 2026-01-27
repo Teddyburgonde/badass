5d53faf84b7d2cee9b2d70de015ebba214b5abae
teddy <Teddyburgonde>
2026-01-27 09:47:48 +0100

    Config p3 ok, to do test

--- badass/projet/p3/scripts/vxlan_vtep3.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-ip link add vxlan0 type vxlan id 42 local 1.1.1.4
-ip link set dev vxlan0 up
-ip link add br0 type bridge
-ip link set br0 up
-ip link set dev vxlan0 master br0
-ip link set dev eth0 master br0
-ip addr add 172.16.1.4/24 dev br0
