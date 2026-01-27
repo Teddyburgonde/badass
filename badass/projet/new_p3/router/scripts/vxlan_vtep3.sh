b2aa934a93c4ddbf6ab3ba3e678f522171cec234
teddy <Teddyburgonde>
2026-01-27 08:15:08 +0100

    Modified structure for p3

---
+++ badass/projet/new_p3/router/scripts/vxlan_vtep3.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Créer l'interface VXLAN avec VNI 10
+ip link add vxlan10 type vxlan id 10 local 1.1.1.4 dstport 4789 nolearning
+
+# Activer l'interface VXLAN 
+ip link set dev vxlan10 up
+
+# Créer le bridge
+ip link add br0 type bridge
+ip link set br0 up
+
+# Ajouter VXLAN au bridge
+ip link set dev vxlan10 master br0
+
+# Ajouter eth1(interface vers le Host) au bridge
+ip link set dev eth1 master br0
+ip link set dev eth1 up
